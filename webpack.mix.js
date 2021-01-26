a613ce6fa00a91e86c7c989dc2c74c34b46bf53f
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-01-25 21:06:54 -0300

    Adding new eslint config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig({
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        test: /\.(js)?$/
+      },
+    ]
+  }
+})
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
